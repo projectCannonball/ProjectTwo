84406447db773e303b237093f81016cd95ef18f6
Bryan Isles <bryanisles@gmail.com>
2018-01-06 13:03:45 -0600

    Fixed reference to 'races'

--- models/raceModel.js
+++ models/raceModel.js
@@ -3,19 +3,19 @@ var orm = require("../config/orm.js");
 var race = {
 	// Select All Data
 	selectAll: (cb) => {
-		orm.selectAll("USERS",(res) => {
+		orm.selectAll("races",(res) => {
 			cb(res);
 		});
 	},
 	// Create a Single Entry
 	insertOne: (cols, vals, cb) => {
-		orm.insertOne("USERS", cols, vals, (res) => {
+		orm.insertOne("races", cols, vals, (res) => {
 			cb(res);
 		});
 	},
 	// Update a Single Entry
 	updateOne: (objColVals, condition, cb) => {
-		orm.updateOne("USERS", objColVals, condition, (res) => {
+		orm.updateOne("races", objColVals, condition, (res) => {
 			cb(res);
 		});
 	}
