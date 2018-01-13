80bcec553e1b7fd8349b032725284e7157cb56f1
AndrewD14 <Leingod2005@gmail.com>
2018-01-13 02:44:10 -0600

    Added in the dynamic modal list of races to join to the pages.

--- config/connection.js
+++ config/connection.js
@@ -22,7 +22,7 @@ var getConnection = function(){
         console.error("error connecting: " + error);
         return reject();
       }
-      console.log("connected as id " + connection.threadId);
+
       return resolve(connection);
     });
   });
---
+++ public/assets/js/join.js
@@ -0,0 +1,13 @@
+$("#joinBtn").on("click", function(){
+    $.ajax({
+        url: "/getRaceList/"+$("#currRaceID").attr("value"),
+        method:"GET"
+    }).done(function(data){
+        $("#raceList").empty();
+        for(i in data){
+            $("#raceList").append('<div class="form-group"><label><a href="/join/'+$("#currUserID").attr("value")+'/'+data[i].ID+'">'+data[i].raceName+
+            '</a></label><label>'+data[i].startDate+'</label><label>'+data[i].endDate+'</label><label>'+data[i].distance+' miles</label></div>');
+        }
+    });
+    
+});
