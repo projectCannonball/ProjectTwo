3692564a722d4a7806bbd9e2f13b25d3d0947937
AndrewD14 <Leingod2005@gmail.com>
2018-01-13 06:45:35 -0600

    Making a column to not allow nulls.

--- db/create_table.sql
+++ db/create_table.sql
@@ -26,7 +26,7 @@ CREATE TABLE Races (
     endDate DATE NOT NULL,
     startLoc VARCHAR(255) NOT NULL,
     endLoc VARCHAR(255) NOT NULL,
-    type VARCHAR(255),
+    type VARCHAR(255) NOT NULL,
     creator_id INT NOT NULL,
     create_ts TIMESTAMP NOT NULL DEFAULT current_timestamp,
     update_ts TIMESTAMP NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
